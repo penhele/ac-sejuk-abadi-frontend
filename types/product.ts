7a2a37a54860cacbefac049ad5ec7bee3b8eb0d2
Stephen Helenus <stephenhelk@gmail.com>
2026-03-07 10:31:49 +0700

    feat: initial commit

---
+++ types/product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  id: string;
+  name: string;
+  description: string;
+  type: string;
+  price: number;
+  pk: number;
+  quantity: number;
+  createdAt: string;
+  updatedAt: string;
+  brandId: string;
+};
