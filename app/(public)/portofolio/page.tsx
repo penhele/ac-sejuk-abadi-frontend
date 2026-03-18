8c2d0bc69f1dd4cc77ed72c34bf2db9828cc6e71
Stephen Helenus <stephenhelk@gmail.com>
2026-03-18 14:59:06 +0700

    feat: add portofolio list

---
+++ app/(public)/portofolio/page.tsx
@@ -0,0 +1,9 @@
+import Portofolio from "@/components/pages/portofolio/portofolio";
+
+export default function page() {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Portofolio />
+    </div>
+  );
+}
---
+++ components/pages/portofolio/portofolio-detail.tsx
@@ -0,0 +1,3 @@
+export default function PortofolioDetail() {
+  return <div className=""></div>;
+}
