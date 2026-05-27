f9b52a7cb88c3e4c0376b70a1d33633879f3fa00
Stephen Helenus <stephenhelk@gmail.com>
2026-05-27 16:25:23 +0700

    edit combobox field

--- components/fields/combobox-field.tsx
+++ components/fields/combobox-field.tsx
@@ -9,6 +9,14 @@ import {
 import { cn } from "@/lib/utils";
 import { Label } from "../ui/label";
 import { FieldInfo } from "../field-info";
+import { X } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+} from "../ui/input-group";
 
 export default function ComboboxField({
   label,
@@ -35,17 +43,60 @@ export default function ComboboxField({
         value={field.state.value}
         onValueChange={field.handleChange}
       >
-        <ComboboxInput placeholder="Select " />
+        <ComboboxInput placeholder="Select products" />
+
         <ComboboxContent>
           <ComboboxList>
-            {(item) => (
+            {items.map((item) => (
               <ComboboxItem key={item.id} value={item.id}>
                 {item.name}
-              </ComboboxItem>                                     
-            )}
+              </ComboboxItem>
+            ))}
           </ComboboxList>
         </ComboboxContent>
       </Combobox>
+
+      <div className="flex flex-col gap-between-items-xs">
+        {field.state.value?.map((id) => {
+          const selectedItem = items.find((i) => i.id === id);
+
+          return (
+            <InputGroup key={id}>
+              <InputGroupInput
+                readOnly
+                value={selectedItem ? selectedItem.name : id}
+              />
+
+              <InputGroupAddon align={"inline-end"}>
+                <InputGroupButton
+                  size={"icon-xs"}
+                  onClick={() => {
+                    field.handleChange(
+                      field.state.value.filter((value) => value !== id),
+                    );
+                  }}
+                >
+                  <X />
+                </InputGroupButton>
+              </InputGroupAddon>
+            </InputGroup>
+          );
+        })}
+      </div>
+
+      {/* <div className="flex flex-col mt-2 gap-1">
+        {field.state.value?.map((id, index) => {
+          // Cari item di dalam array 'items' yang id-nya cocok
+          const selectedItem = items.find((i) => i.id === id);
+
+          // Tampilkan name jika ketemu, kalau tidak ketemu fallback ke id
+          return (
+            <span key={id || index} className="text-sm text-muted-foreground">
+              {selectedItem ? selectedItem.name : id}
+            </span>
+          );
+        })}
+      </div> */}
     </div>
   );
 }
