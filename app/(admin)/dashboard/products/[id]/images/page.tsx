f94ec208501a2a1ead3fcd7e424a9f10f33fa99b
Stephen Helenus <stephenhelk@gmail.com>
2026-05-28 20:49:36 +0700

    fix: menghapus yang tidak digunakan

--- app/(admin)/dashboard/products/[id]/images/page.tsx
+++
@@ -1,5 +0,0 @@
-import ProductImagePage from "@/components/pages/dashboard/product/product-image-page";
-
-export default function Page() {
-  return <ProductImagePage />;
-}
--- app/(admin)/dashboard/products/[id]/images/upload/page.tsx
+++
@@ -1,5 +0,0 @@
-import UploadProductImagePage from "@/components/pages/dashboard/product/upload-product-image-page";
-
-export default function Page() {
-  return <UploadProductImagePage />;
-}
