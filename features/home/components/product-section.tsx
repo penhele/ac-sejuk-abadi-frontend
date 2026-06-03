d2e2f5ed979fad906df1a231aa6666909a0e281d
Stephen Helenus <stephenhelk@gmail.com>
2026-06-03 12:03:20 +0700

    fix suspense home page

--- features/home/components/product-section.tsx
+++ features/home/components/product-section.tsx
@@ -1,14 +1,21 @@
 import CarouselProduct from "@/components/carousel/carousel-product";
+import ErrorFallback from "@/components/fallback/error-fallback";
 import ProductGrid from "@/components/grid/product-grid";
 import { Header } from "@/components/header";
 import { ROUTES } from "@/constants/routes";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default function ProductSection() {
   return (
     <div className="">
       <Header title="Produk" href={ROUTES.SHOP} />
 
-      <ProductGrid length={10} />
+      <ErrorBoundary fallback={<ErrorFallback />}>
+        <Suspense>
+          <ProductGrid length={10} />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
--- features/home/home-page.tsx
+++ features/home/home-page.tsx
@@ -1,39 +1,7 @@
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import {
-  AirVent,
-  ArrowUpRight,
-  ShoppingCart,
-  Sparkles,
-  Store,
-} from "lucide-react";
-import StatsCard from "./components/stats-card";
 import AboutSection from "./components/about-section";
-import CarouselProduct from "@/components/carousel/carousel-product";
 import ProductSection from "./components/product-section";
 
 export default function HomePage() {
-  const stats = [
-    {
-      label: "Products",
-      value: "1000",
-      description: "Produk yang sudah terjual",
-      Icon: ShoppingCart,
-    },
-    {
-      label: "Brands",
-      value: "10",
-      description: "Merek AC dan perlengkapan tersedia",
-      Icon: Sparkles,
-    },
-    {
-      label: "Projects",
-      value: "100",
-      description: "Proyek instalasi berhasil dituntaskan",
-      Icon: AirVent,
-    },
-  ];
-
   return (
     <div className="space-y-between-section">
       <AboutSection />
