22c1d1f0c0c184721c9347dcd8dd89ccc6631c60
Stephen Helenus <stephenhelk@gmail.com>
2026-06-05 12:06:34 +0700

    give suspense

--- app/(admin)/dashboard/products/page.tsx
+++ app/(admin)/dashboard/products/page.tsx
@@ -1,5 +1,10 @@
 import ProductPage from "@/components/pages/dashboard/product/product-page";
+import { Suspense } from "react";
 
 export default function page() {
-  return <ProductPage />;
+  return (
+    <Suspense>
+      <ProductPage />
+    </Suspense>
+  );
 }
