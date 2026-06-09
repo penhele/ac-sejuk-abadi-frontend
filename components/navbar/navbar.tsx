bcdda3c54d8c3d49ecdbc6f3ff058d75a73019c7
Stephen Helenus <stephenhelk@gmail.com>
2026-06-09 15:04:32 +0700

    feat: add logo

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -6,22 +6,29 @@ import NavItem from "./nav-item";
 import { Toggle } from "../ui/toggle";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { ROUTES } from "@/constants/routes";
+import { useCompany } from "@/features/company/hooks/use-company";
 
 export default function Navbar() {
   const { theme, setTheme } = useTheme();
+  const { data: company } = useCompany();
 
   return (
     <div className="sticky top-0 z-50 bg-white dark:bg-background h-16">
       <div className="max-w-7xl mx-auto flex flex-row items-center justify-between">
         <div className="flex lg:flex-row flex-col lg:justify-between items-center py-2">
           <div className="flex w-full lg:w-fit xs:justify-between justify-center flex-row gap-16 items-center ">
-            <Link href={"/"} className="relative h-12 w-28">
-              <Image
-                src="/logo.png"
-                alt="logo"
-                fill
-                className="object-contain"
-              />
+            <Link href={ROUTES.HOME} className="relative h-12 w-28">
+              {company?.logo_url ? (
+                <Image
+                  src={company?.logo_url}
+                  alt="logo-company"
+                  fill
+                  className="object-contain"
+                />
+              ) : (
+                <span>logo</span>
+              )}
             </Link>
 
             <div className="hidden xs:block">
