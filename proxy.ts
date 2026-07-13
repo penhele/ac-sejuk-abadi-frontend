e51c7fe3f14fbd9b17e256c5360d929221affcbe
Stephen Helenus <stephenhelk@gmail.com>
2026-07-13 19:33:27 +0700

    fix proxy

--- proxy.ts
+++ proxy.ts
@@ -1,21 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 
 function getRoleFromToken(token: string) {
-  try {
-    const base64Url = token.split(".")[1];
-    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-    const jsonPayload = decodeURIComponent(
-      atob(base64)
-        .split("")
-        .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
-        .join(""),
-    );
-    const payload = JSON.parse(jsonPayload);
+  const base64Url = token.split(".")[1];
+  if (!base64Url) return null;
 
-    return payload?.role;
-  } catch (e) {
-    return null;
+  let base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+
+  while (base64.length % 4) {
+    base64 += "=";
   }
+
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+      .join(""),
+  );
+  const payload = JSON.parse(jsonPayload);
+
+  return payload?.role;
 }
 
 export function proxy(request: NextRequest) {
